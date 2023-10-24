4a980b6b0ebfeee99a0bb4065fc33b652afcbd2b
Robert Knight <robertknight@gmail.com>
2023-10-24 11:55:31 +0100

    Clarify that Slack channel is specified via ID, rather than name

--- dependabot_batch_review/slack.py
+++ dependabot_batch_review/slack.py
@@ -9,15 +9,18 @@ class SlackClient:
     def __init__(self, token: str):
         self.token = token
 
-    def post_message(self, channel: str, message: str):
+    def post_message(self, channel_id: str, message: str):
         """
         Post a message to a Slack channel.
 
         See https://api.slack.com/methods/chat.postMessage.
+
+        :param channel_id: Channel ID, available from the bottom of a channel's "About" dialog
+        :param message: Message text using Slack's "mrkdwn" format
         """
 
         body = {
-            "channel": channel,
+            "channel": channel_id,
             "text": message,
         }
         self._call(body)
